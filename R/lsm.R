a140b843fccaa8656eaacceb642e9868f61190ce
jlvia1191 <jlvia1191@gmail.com>
2017-11-28 00:14:44 -0500

    commit

--- R/lsm.R
+++ R/lsm.R
@@ -1,7 +1,7 @@
 # lsm.R
 
 #' @title Estimation of the log Likelihood of the Saturated Model
-#' @description When the values of the outcome variable Y are either 0 or 1, the function lsm() calculates the estimation of the log likelihood in the saturated model. This model is characterized by Llinas (2006, ISSN:2389-8976) in section 2.3 through the assumptions 1 and 2. The function LogLik() works (almost perfectly) when the number of independent variables K is high, but for small K it calculates wrong values in some cases. For this reason, when Y is dichotomous and the data are grouped in J populations, it is recommended to use the function lsm() because it works very well for all K.
+#' @description When the values of the outcome variable Y are either 0 or 1, the function lsm() calculates the estimation of the log likelihood in the saturated model. This model is characterized by Llinas (2006, ISSN:2389-8976) in section 2.3 through the assumptions 1 and 2. If Y is dichotomous and the data are grouped in J populations, it is recommended to use the function lsm() because it works very well for all K.
 
 #' @param formula An expression of the form y ~ model, where y is the outcome variable (binary or dichotomous: its values are 0 or 1).
 #' @param data an optional data frame, list or environment (or object coercible by as.data.frame to a data frame) containing the variables in the model. If not found in data, the variables are taken from environment(formula), typically the environment from which lsm() is called.
@@ -30,12 +30,12 @@
 #'
 #' # Other case.
 #'
-#'   y <- c(1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1)
-#'  x1 <-	c(2, 2, 2, 5, 5, 5, 5, 8, 8, 11, 11, 11)
-#'  x2 <-	c(3, 3, 3, 6, 6, 6, 6, 9, 9, 12, 12, 12)
-#'  x3 <-	c(4, 4, 4, 7, 7, 7, 7, 10, 10, 13, 13, 13)
-#'  x4 <-	c(1, 1, 1, 9, 9, 9, 9, 4, 4, 2, 2, 2)
-#'  x5 <-	c(5, 5, 5, 6, 6, 6, 6, 7, 7, 8, 8, 8)
+#'   y <- as.factor(c(1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1))
+#'  x1 <-	as.factor(c(2, 2, 2, 5, 5, 5, 5, 8, 8, 11, 11, 11))
+#'  x2 <-	as.factor(c(3, 3, 3, 6, 6, 6, 6, 9, 9, 12, 12, 12))
+#'  x3 <-	as.factor(c(4, 4, 4, 7, 7, 7, 7, 10, 10, 13, 13, 13))
+#'  x4 <-	as.factor(c(1, 1, 1, 9, 9, 9, 9, 4, 4, 2, 2, 2))
+#'  x5 <-	as.factor(c(5, 5, 5, 6, 6, 6, 6, 7, 7, 8, 8, 8))
 #'
 #'   data <- data.frame (y, x1, x2, x3, x4, x5)
 #'  lsm(y ~ x1 + x2 + x3 + x4 + x5, data)
@@ -46,17 +46,18 @@
 #' @export
 #' @import stats
 
+ lsm <- function(formula,data) {
+    L <-as.formula(formula)
+    mf <- model.frame(formula = L, data = data)
+    res <-do.call(rbind, (tapply(as.vector(mf[, 1]), t(apply((mf[, -1,drop =FALSE]), 1, paste0,collapse = "")),function(x) c(z = sum(as.numeric(x)), n = length(as.numeric(x)),p = mean(as.numeric(x))))))
+    sj <- (res[, 1] * log(res[, 3]) + (res[, 2] - res[, 1]) * log(1 - res[, 3]))
+    sat <- sum(ifelse ((res[, 3]) == 0 | (res[, 3]) == 1, 0, sj))
+    na <- list(log_Likelihood = sat, populations = length(res) / 3)
+    return(na)
 
-lsm <- function(formula,data)
-{
-  L <- as.formula(formula)
-  mf <- model.frame(formula = L, data = data)
-  res <- do.call(rbind, tapply(mf[, 1], t(apply(t(t(mf[, -1])), 1, paste0,      collapse = "")), function(x) c(p = mean(x), z = sum(x), n = length(x))))
-  sj <- (res[, 2]*log(res[, 1]) + (res[, 3] - res[, 2])*log(1 - res[, 1]))
-  sat <- round(sum(ifelse ( (res[, 1]) == 0 | (res[, 1]) == 1, 0, sj)), 4)
-  na <- list(log_Likelihood = sat, populations = length(res) / 3)
-  return(na)
-}
+  }
+
+
 
 
 
